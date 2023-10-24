6ef342c55fb3c4b8d0665adc4db1093feb0a3d0b
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2023-10-24 11:57:07 +0200

    Agregar archivos de proyecto.

---
+++ Controladores/Program.cs
@@ -0,0 +1,16 @@
+﻿using Condicionales02;
+using Condicionales02.Servicios;
+
+class Program
+{
+
+    static void Main(String[] args)
+    {
+
+        MenuInterfaz mi = new MenuImplementacion();
+        int valor01 = mi.pedirNum();
+        int valor02 = mi.pedirNum();
+
+      Console.WriteLine(valor01 * valor01 == valor02);
+    }
+}
---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales02.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+
+        public int pedirNum()
+        {
+            int valor;
+            Console.WriteLine("Introduzca un Valor");
+            valor = Convert.ToInt32(Console.ReadLine());
+            return valor;
+        }
+
+    }
+}
